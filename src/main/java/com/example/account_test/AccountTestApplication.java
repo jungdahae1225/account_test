eeab3a698ed90efd5e1ae70a7ad8f0ebae20b3eb
jungdahae1225 <jungdahae1225@gmail.com>
2022-04-26 02:59:52 +0900

    Initial commit

---
+++ src/main/java/com/example/account_test/AccountTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.account_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountTestApplication.class, args);
+    }
+
+}
